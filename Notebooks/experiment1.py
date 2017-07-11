066084015d302c2f14fe79405eadb10b5df073ca
Stephen Graham <s1601939@sms.ed.ac.uk>
2017-07-11 12:22:26 +0100

    simplifying OOV check
    and probably fixing it…

--- Notebooks/experiment1.py
+++ Notebooks/experiment1.py
@@ -50,7 +50,7 @@ def get_similarities(this_model, joke):
     # remove unwanted tags
     joke_words = [w for w in joke_words if w.split('|')[1] not in stop_tags]
     # remove OOV words
-    joke_words = [w for w in joke_words if this_model.in_vocab(w.split('|')[0])]
+    joke_words = [w for w in joke_words if this_model.in_vocab(w)]
 
     pairs = list(itertools.combinations(joke_words,2))
     for (left_word,right_word) in pairs:
